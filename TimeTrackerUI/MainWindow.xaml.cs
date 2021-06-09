6555144e90e489b19e48077196fa4cdb47316eb9
Ayça Yıldırım <eaycayildirim@gmail.com>
2021-06-09 12:00:01 +0300

    Feat: Add datetime

--- TimeTrackerUI/MainWindow.xaml.cs
+++ TimeTrackerUI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace TimeTrackerUI
 {
@@ -23,7 +24,30 @@ namespace TimeTrackerUI
         public MainWindow()
         {
             InitializeComponent();
-            CurrentTimeLabel.Content = DateTime.Now.ToString("MM/dd/yyyy HH:mm");
+            //CurrentTimeLabel.Content = DateTime.Now.ToString("MM/dd/yyyy HH:mm");
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0,0,1);
+            timer.Tick += new EventHandler(UpdateTimerTick);
+            timer.Start();
+        }
+
+        private void UpdateTimerTick(object sender, EventArgs e)
+        {
+            CurrentTimeLabel.Content = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss");
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            SelectionCombobox.Items.Add(AddTextBox.Text);
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            SelectionCombobox.Items.RemoveAt(SelectionCombobox.Items.IndexOf(AddTextBox.Text));
         }
     }
 }
