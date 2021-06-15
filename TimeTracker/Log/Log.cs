623e68efa1f0e83f7a2852adf0bb5fb49120af3d
Ayça Yıldırım <eaycayildirim@gmail.com>
2021-06-15 11:20:47 +0300

    commit

---
+++ TimeTracker/Log/Log.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using nsElapsedTime;
+
+namespace nsLog
+{
+    class Log
+    {
+        public string ReturnTheLog(Stopwatch stopwatch)
+        {
+            ElapsedTime elapsedTime = new ElapsedTime(stopwatch);
+            if (!stopwatch.IsRunning)
+            {
+                return ";" + DateTime.Now + ";" + elapsedTime.ReturnElapsedTime() + "; Finished" + "\n";
+            }
+            else
+            {
+                return ";" + DateTime.Now + "; Started" + "\n";
+            }
+        }
+    }
+}
