0fdf21a4ed965e0db010a4bcf46deeffafd37fd6
Ayça Yıldırım <eaycayildirim@gmail.com>
2021-07-06 17:19:21 +0300

    Feat: Add tracker tests

--- UnitTestTimeTracker/TestPauseButton.cs
+++ UnitTestTimeTracker/TestPauseButton.cs
@@ -28,40 +28,6 @@ namespace UnitTestTimeTracker
             Assert.IsTrue(_tasks[selection].IsPressed());
             Assert.IsFalse(_tasks[selection].IsRunning());
         }
-        [TestMethod]
-        public void TestStartThePausedTask()
-        {
-            //Given
-            int selection = 0;
-
-            //When
-            _tracker.UpdatePressedButtonsMock(selection);
-            _tracker.PauseTheTask(selection);
-            _tracker.UpdatePressedButtonsMock(selection);
-
-            //Then
-            Assert.IsTrue(_tasks[selection].IsPressed());
-            Assert.IsTrue(_tasks[selection].IsRunning());
-        }
-
-        [TestMethod]
-        public void TestStartTaskWithoutFinishThePausedOne()
-        {
-            //Given
-            int firstSelection = 0;
-            int secondSelection = 1;
-
-            //When
-            _tracker.UpdatePressedButtonsMock(firstSelection);
-            _tracker.PauseTheTask(firstSelection);
-            _tracker.UpdatePressedButtonsMock(secondSelection);
-
-            //Then
-            Assert.IsFalse(_tasks[firstSelection].IsPressed());
-            Assert.IsFalse(_tasks[firstSelection].IsRunning());
-            Assert.IsTrue(_tasks[secondSelection].IsPressed());
-            Assert.IsTrue(_tasks[secondSelection].IsRunning());
-        }
 
         private List<TrackerTask> _tasks;
         private TrackerMock _tracker;
