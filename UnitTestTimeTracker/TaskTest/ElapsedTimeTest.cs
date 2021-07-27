56799e3e141e57ac819f6d4c040569020160430d
Ayça Yıldırım <eaycayildirim@gmail.com>
2021-07-27 12:16:35 +0300

    feat: Add new elapsed time tests

--- UnitTestTimeTracker/TaskTest/ElapsedTimeTest.cs
+++ UnitTestTimeTracker/TaskTest/ElapsedTimeTest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
 using nsElapsedTime;
-using System.Diagnostics;
 
 namespace UnitTestTimeTracker
 {
@@ -8,29 +9,56 @@ namespace UnitTestTimeTracker
     public class ElapsedTimeTest
     {
         [TestMethod]
-        public void Start_ElapsedTimeIsRunning_True()
+        public void Start_ElapsedTimeIsRunning()
         {
             //Given
             ElapsedTime elapsedTime = new ElapsedTime();
+            var miliseconds = 1000;
+            var actual = "00:00:01";
 
             //When
             elapsedTime.Start();
+            Thread.Sleep(miliseconds);
 
             //Then
-            Assert.AreEqual(true, elapsedTime.IsRunning());
+            Assert.AreEqual(elapsedTime.ReturnElapsedTime(), actual);
+            Assert.IsTrue(elapsedTime.IsRunning());
         }
 
         [TestMethod]
-        public void Stop_ElapsedTimeIsRunning_False()
+        public void Restart_ElapsedTimeIsRestarted()
         {
             //Given
             ElapsedTime elapsedTime = new ElapsedTime();
+            var miliseconds = 2000;
+            var actual = "00:00:00";
 
             //When
+            elapsedTime.Start();
+            Thread.Sleep(miliseconds);
+            elapsedTime.Restart();
+
+            //Then
+            Assert.AreEqual(elapsedTime.ReturnElapsedTime(), actual);
+            Assert.IsTrue(elapsedTime.IsRunning());
+        }
+
+        [TestMethod]
+        public void Stop_ElapsedTimeIsRunning()
+        {
+            //Given
+            ElapsedTime elapsedTime = new ElapsedTime();
+            var miliseconds = 3000;
+            var actual = "00:00:03";
+
+            //When
+            elapsedTime.Start();
+            Thread.Sleep(miliseconds);
             elapsedTime.Stop();
 
             //Then
-            Assert.AreEqual(false, elapsedTime.IsRunning());
+            Assert.AreEqual(elapsedTime.ReturnElapsedTime(), actual);
+            Assert.IsFalse(elapsedTime.IsRunning());
         }
     }
 }
